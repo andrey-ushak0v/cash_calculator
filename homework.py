1444be8db25dbaee87dca49877e14e5fa0372381
Ушаков <andrey.ushakov.00@inbox.ru>
2021-08-07 13:16:36 +0300

    прйдены все тесты

--- homework.py
+++ homework.py
@@ -1,15 +1,18 @@
 import datetime as dt
+from typing import Optional
 
 
 class Record:
     def __init__(self,
                  amount: int,
                  comment: str,
-                 date: str = dt.date.today().strftime('%d.%m.%Y')) -> None:
+                 date: Optional[str] = None) -> None:
         self.amount = amount
         self.comment = comment
-        self.date = dt.datetime.strptime(date, '%d.%m.%Y').date()
-
+        self.date: dt.date = dt.date.today()
+        if date is not None:
+            self.date = dt.datetime.strptime(date, '%d.%m.%Y').date()
+       
 
 class Calculator:
     def __init__(self, limit):
@@ -39,8 +42,8 @@ class Calculator:
 
 class CashCalculator(Calculator):
 
-    USD_RATE = 70
-    EURO_RATE = 85
+    USD_RATE = 70.00
+    EURO_RATE = 85.00
 
     def get_today_cash_remained(self, currency):
         limit = self.limit
@@ -49,17 +52,17 @@ class CashCalculator(Calculator):
                    'usd': (today_stats / self.USD_RATE,
                            limit / self.USD_RATE, 'USD'),
                    'eur': (today_stats / self.EURO_RATE,
-                           limit / self.EURO_RATE, 'euro')}
+                           limit / self.EURO_RATE, 'Euro')}
         money, limit_money, rate = my_dict[currency]
         balance = abs(limit_money - money)
         if limit > today_stats:
-            return(f'на сегодня осталось {balance: .2f}'
-                   f'{rate}')
+            return(f'На сегодня осталось{balance: .2f}'
+                   f' {rate}')
         elif limit == today_stats:
-            return('Денег нет, держись!')
+            return('Денег нет, держись')
         elif today_stats > limit:
-            return(f'денег нет, держись: твой долг - {balance: .2f}'
-                   f'{rate}')
+            return(f'Денег нет, держись: твой долг -{balance: .2f}'
+                   f' {rate}')
 
 
 class CaloriesCalculator(Calculator):
@@ -68,24 +71,24 @@ class CaloriesCalculator(Calculator):
         today_stats = self.get_today_stats()
         if limit > today_stats:
             new_limit_cal = limit - today_stats
-            return(f'можно съесть что то еще,'
-                   f'не более {new_limit_cal} каллорий')
+            return(f'Сегодня можно съесть что-нибудь ещё, но с общей'
+                   f' калорийностью не более {new_limit_cal} кКал')
         elif today_stats >= limit:
             return('Хватит есть!')
 
 
 cash_calculator = CashCalculator(1000)
+cash_calculator.add_record(Record(amount=400, comment="beer",
+                                  date="07.08.2021"))
 cash_calculator.add_record(Record(amount=100, comment="beer",
-                                  date="06.08.2021"))
-cash_calculator.add_record(Record(amount=10, comment="beer",
-                                  date="06.08.2021"))
-cash_calculator.add_record(Record(amount=890, comment="tea",
-                                  date="06.08.2021"))
-print(cash_calculator.get_today_cash_remained('rub'))
+                                  date="07.08.2021"))
+cash_calculator.add_record(Record(amount=5, comment="tea",
+                                  date="07.08.2021"))
+print(cash_calculator.get_today_cash_remained('rub'))           
 calories_calculator = CaloriesCalculator(2000)
-calories_calculator.add_record(Record(amount=1000, comment="pizza",
-                                      date="06.08.2021"))
+calories_calculator.add_record(Record(amount=180, comment="pizza",
+                                      date="07.08.2021"))
 calories_calculator.add_record(Record(amount=1, comment="pizza",
-                                      date="06.08.2021"))
+                                      date="07.08.2021"))
 calories_calculator.add_record(Record(amount=500, comment="pizza",
-                                      date="06.08.2021"))
+                                      date="07.08.2021"))
